f91d05c4648336c013df80c5200457d722330578
Ondřej Caha <cahaondra@gmail.com>
2026-05-16 12:08:08 +0900

    Added fullscreen window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod camera;
+
+use bevy::{
+    prelude::*,
+    window::{WindowMode, WindowPlugin},
+};
+
+use crate::camera::MainCameraPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Bomberman".to_string(),
+                        mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                        ..default()
+                    }),
+                    ..Default::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_plugins(MainCameraPlugin)
+        .run();
 }
