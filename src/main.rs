bdf91a71d88a910d478279d99afbe68cdf561edb
Ondřej Caha <cahaondra@gmail.com>
2026-06-21 11:53:45 +0900

    Added tilesets

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,19 @@ use bevy::{
 
 use crate::camera::MainCameraPlugin;
 
+fn get_assets_path() -> String {
+    // TODO make it dependent on build directory
+    "assets".to_string()
+}
+
 fn main() {
     App::new()
         .add_plugins(
             DefaultPlugins
+                .set(AssetPlugin {
+                    file_path: get_assets_path().into(),
+                    ..default()
+                })
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Bomberman".to_string(),
