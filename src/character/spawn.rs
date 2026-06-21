8d1674492a27fed31ec730b84eba501623fd3758
Ondřej Caha <cahaondra@gmail.com>
2026-06-21 19:45:31 +0900

    Removed spawning dependency of character to camera scale

--- src/character/spawn.rs
+++ src/character/spawn.rs
@@ -1,13 +1,9 @@
 use bevy::prelude::*;
 
 use crate::{
-    assets::{
-        TILESET_TILE_SIZE,
-        character_tileset::{CharacterTileType, prepare_character_tileset_handles},
-    },
+    assets::character_tileset::{CharacterTileType, prepare_character_tileset_handles},
     character::movement::CharacterMovement,
     position::WorldPosition,
-    util::CameraScale,
 };
 
 #[derive(Component)]
@@ -17,12 +13,10 @@ pub fn spawn_character(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-    camera_scale: Res<CameraScale>,
 ) {
     let character_tileset_handles =
         prepare_character_tileset_handles(&asset_server, &mut atlas_layouts);
 
-    let scale = camera_scale.0;
     let spawn_position = Vec2::new(-8.0, 6.0);
     commands.spawn((
         Character,
@@ -34,12 +28,7 @@ pub fn spawn_character(
                 index: CharacterTileType::Standing.index(),
             },
         ),
-        Transform::from_xyz(
-            spawn_position.x * TILESET_TILE_SIZE.x as f32 * scale,
-            spawn_position.y * TILESET_TILE_SIZE.y as f32 * scale,
-            1.0,
-        )
-        .with_scale(Vec3::splat(scale)),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
         CharacterMovement::default(),
     ));
 }
